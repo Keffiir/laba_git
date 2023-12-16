319dbb07fa75139be453a49c672152eb6cfaaa2d
Ivan Nikiforov <nikiforov.ivan555@gmail.com>
2023-12-16 21:17:24 +0300

    Final commit

--- 6 laba.py
+++ 6 laba.py
@@ -1,17 +1,19 @@
 # 15. Написать программу для вычисления общего размера каталога и его подкаталогов.
 # Вывести в текстовом файле иерархию каталогов с их размером.
-import shutil
 import os
 
 def get_subdirectories(directory):
+    # Возвращаем список названий подкаталогов для заданого каталога
     return [name for name in os.listdir(directory) if os.path.isdir(os.path.join(directory, name))]
 
-total_size = 0
 main_directory = '/Users/nikiforovivan/Desktop/УЧЕБА ВУЗ/Инфа/newcatalog/'
-my_file = open(os.path.join(main_directory, "info.txt"), "w+")
+# Создаем файл info.txt в основном каталоге
+info_file = open(os.path.join(main_directory, "info.txt"), "w+")
+# Записываем в файл info.txt называние основного каталога и его вес
+info_file.write(str(os.path.dirname(main_directory)) + " " + str(os.path.getsize(main_directory)) + " bytes" + "\n")
+# В цикле for проходимся по всем названиям подкаталогов
 for file in get_subdirectories(main_directory):
+    # Создаем полный путь для каждого из названий подкаталогов
     current_path = os.path.join(main_directory, file)
-    my_file.write(file + " " + str(os.path.getsize(current_path)) + "bit" + "\n")
-    total_size += os.path.getsize(current_path)
-
-print(total_size)
+    # Записываем в файл info_file название подкаталога и его размер
+    info_file.write("  - " + file + " " + str(os.path.getsize(current_path)) + " bytes" + "\n")
